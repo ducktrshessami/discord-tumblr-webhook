6fd4ab21462bf76c31b3b7f263a4505aabb2dc0a
ducktrshessami <jwee761@gmail.com>
2023-09-06 11:51:36 -0700

    fix: misread last_checked continues

--- src/config.js
+++ src/config.js
@@ -8,9 +8,15 @@ import { fileURLToPath } from "url";
 const path = fileURLToPath(new URL("../config.json", import.meta.url));
 
 export function getLastTimestamp() {
-    if (existsSync(path)) {
-        const config = JSON.parse(readFileSync(path, { encoding: "utf8" }));
-        return new Date(config.last_checked).getTime();
+    try {
+        if (existsSync(path)) {
+            const config = JSON.parse(readFileSync(path, { encoding: "utf8" }));
+            return new Date(config.last_checked).getTime();
+        }
+    }
+    catch (err) {
+        console.log(err);
+        console.log("Failed to read last_checked config");
     }
     return Date.now();
 }
