8785c138875baef4499f1e0f69e755b0d2aaff58
ducktrshessami <jwee761@gmail.com>
2023-09-05 22:59:11 -0700

    feat: repost queue

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { getNewPosts, getPostImageURLs } from "./tumblr.js";
+import { repost } from "./discord.js";
+import { setTimeout } from "timers/promises";
+import { QUEUE_INTERVAL } from "./constants.js";
+
+async function queuedPosts(blog, posts) {
+    if (!posts.length) {
+        console.log("No posts found!");
+        return;
+    }
+    for (let i = 0; i < posts.length; i++) {
+        try {
+            const post = posts[i];
+            const imageUrls = getPostImageURLs(post);
+            if (!imageUrls.length) {
+                continue;
+            }
+            await repost(blog, post, imageUrls);
+            if (i !== posts.length - 1) {
+                await setTimeout(QUEUE_INTERVAL);
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+}
+
+try {
+    const { blog, posts } = await getNewPosts();
+    await queuedPosts(blog, posts);
+}
+catch (err) {
+    console.error(err);
+}
