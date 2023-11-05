f0e213e590694730c713fd711f7eca410aa020d0
Josh Fish <jwee761@gmail.com>
2023-11-05 15:07:02 -0800

    refactor: config

--- src/config.js
+++ src/config.js
@@ -7,20 +7,32 @@ import { fileURLToPath } from "url";
 
 const path = fileURLToPath(new URL("../config.json", import.meta.url));
 
-export function getLastTimestamp() {
+export function getConfig() {
     try {
         if (existsSync(path)) {
             const config = JSON.parse(readFileSync(path, { encoding: "utf8" }));
-            return new Date(config.last_checked).getTime();
+            return {
+                lastChecked: new Date(config.last_checked).getTime(),
+                pastOffset: config.past_offset,
+                pastTime: new Date(config.past_time).getTime()
+            };
         }
     }
     catch (err) {
         console.log(err);
-        console.log("Failed to read last_checked config");
+        console.log("Failed to read config");
     }
-    return Date.now();
+    const now = Date.now();
+    return {
+        lastChecked: now,
+        pastOffset: 0,
+        pastTime: now
+    };
 }
 
-export function writeTimestamp(timestamp = new Date()) {
-    writeFileSync(path, `{ "last_checked": "${timestamp.toISOString()}" }\n`, { encoding: "utf8" });
+export function writeConfig({ lastChecked, pastOffset, pastTime } = {}) {
+    lastChecked ??= new Date();
+    pastOffset ??= 0;
+    pastTime ??= new Date();
+    writeFileSync(path, `{ "last_checked": "${lastChecked.toISOString()}", "past_offset": ${pastOffset}, "past_time": "${pastTime}" }\n`, { encoding: "utf8" });
 }
--- src/tumblr.js
+++ src/tumblr.js
@@ -1,14 +1,14 @@
 import { Client } from "tumblr.js";
 import { JSDOM } from "jsdom";
 import { POST_LIMIT } from "./constants.js";
-import { getLastTimestamp, writeTimestamp } from "./config.js";
+import { getConfig, writeConfig } from "./config.js";
 
 const client = new Client({ consumer_key: process.env.TUMBLR_KEY });
 
 export async function getNewPosts() {
-    const lastTimestamp = getLastTimestamp();
+    const lastTimestamp = getConfig();
     const { blog, posts } = await client.blogPosts(process.env.TARGET_BLOG, { limit: POST_LIMIT });
-    writeTimestamp();
+    writeConfig();
     return {
         blog,
         posts: posts.filter(post => post.timestamp * 1000 > lastTimestamp).sort((a, b) => a.timestamp - b.timestamp)
