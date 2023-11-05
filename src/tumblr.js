be070cacf3a6cd1f42074afe60422cb4a7a1e2de
Josh Fish <jwee761@gmail.com>
2023-11-05 15:59:00 -0800

    feat: older post resolution

--- src/tumblr.js
+++ src/tumblr.js
@@ -6,13 +6,32 @@ import { getConfig, writeConfig } from "./config.js";
 const client = new Client({ consumer_key: process.env.TUMBLR_KEY });
 
 export async function getNewPosts() {
-    const lastTimestamp = getConfig();
-    const { blog, posts } = await client.blogPosts(process.env.TARGET_BLOG, { limit: POST_LIMIT });
-    writeConfig();
-    return {
-        blog,
-        posts: posts.filter(post => post.timestamp * 1000 > lastTimestamp).sort((a, b) => a.timestamp - b.timestamp)
-    };
+    const config = getConfig();
+    const { blog, posts: latestPosts } = await client.blogPosts(process.env.TARGET_BLOG, { limit: POST_LIMIT });
+    let posts = latestPosts.filter(post => post.timestamp * 1000 > config.lastChecked).sort((a, b) => a.timestamp - b.timestamp);
+    config.lastChecked = Date.now();
+    if (!posts.length) {
+        const { posts: oldPosts } = await client.blogPosts(process.env.TARGET_BLOG, {
+            limit: POST_LIMIT,
+            offset: config.pastOffset
+        });
+        console.log(oldPosts[0])
+        const [filteredPosts, filterCount] = oldPosts.reduce((data, post) => {
+            if (post.timestamp * 1000 < config.pastTime) {
+                data[0].push(post);
+            }
+            else {
+                data[1]++;
+            }
+            console.log(data, config.pastTime);
+            return data;
+        }, [[], 0]);
+        posts = filteredPosts.sort((a, b) => b.timestamp - a.timestamp).slice(0, 1);
+        config.pastOffset += filterCount || 1;
+        config.pastTime = posts.length ? posts[0].timestamp * 1000 : config.pastTime;
+    }
+    writeConfig(config);
+    return { blog, posts };
 }
 
 export function getPostImageURLs(post) {
